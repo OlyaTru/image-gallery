09abef59d17f37276ab201d696d1d701a2093767
OlyaTru <122283583+OlyaTru@users.noreply.github.com>
2024-09-30 01:59:40 +0200

    feat: add api img to page, add footer

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -4,19 +4,17 @@ let searchWord = "relax";
 const API_KEY = "1FdYHCM9eipd8iknsngmGyQ3568Wkzk-Htq3-quydjs";
 const URL_API = `https://api.unsplash.com/search/photos?client_id=${API_KEY}&page=${pageNumber}&query=${searchWord}`;
 
+const allCardsWrap = document.querySelector('.wrapper-main');
+
 async function getData() {
     const res = await fetch(URL_API);
     const data = await res.json();
-    console.log(data);
-    console.log(data.results)
-    console.log(data.results[0].urls.regular)
     showData(data);
 }
 
 getData();
 
 function showData(data) {
-    const allCardsWrap = document.querySelector('wrapper-main');
     const images = document.createElement("ul");
     images.classList.add("main-images");
     allCardsWrap.appendChild(images);
