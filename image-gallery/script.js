c891e9b2eada3bc7b63911b138b01cc31a944a93
OlyaTru <122283583+OlyaTru@users.noreply.github.com>
2024-09-30 09:03:16 +0200

    feat: add serch to gallery app, by enter and by button, add clear input

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -2,25 +2,28 @@ let pageNumber = randomPage(50);
 let searchWord = "relax";
 
 const API_KEY = "1FdYHCM9eipd8iknsngmGyQ3568Wkzk-Htq3-quydjs";
-const URL_API = `https://api.unsplash.com/search/photos?client_id=${API_KEY}&page=${pageNumber}&query=${searchWord}`;
 
 const allCardsWrap = document.querySelector('.wrapper-main');
 const inputSearch = document.querySelector('.header-search input');
 const btnSearch = document.querySelector('.header-search button');
 
+inputSearch.focus();
+
+function getUrl(key, page, search) {
+    return `https://api.unsplash.com/search/photos?client_id=${API_KEY}&page=${pageNumber}&query=${searchWord}`;
+}
+
 async function getData(url) {
     const res = await fetch(url);
     const data = await res.json();
-
+    let lastPage = data.total_pages;
+    pageNumber = randomPage(lastPage);
     showData(data);
 }
 
-getData(URL_API);
+getData(getUrl(API_KEY, pageNumber, searchWord));
 
 function showData(data) {
-    /*let lastPage = data.total_pages;
-    pageNumber = randomPage(lastPage);*/
-
     allCardsWrap.innerHTML = '';
 
     const images = document.createElement("ul");
@@ -35,18 +38,19 @@ function showData(data) {
     });
 }
 
-/*inputSearch.addEventListener('change', (e) => {
+inputSearch.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        searchWord = e.target.value;
+        getData(getUrl(API_KEY, pageNumber, searchWord));
+    }
+});
 
+btnSearch.addEventListener('click', () => {
     if(inputSearch.value) {
         searchWord = inputSearch.value;
-        getData(URL_API);
+        getData(getUrl(API_KEY, pageNumber, searchWord));
     }
-console.log(searchWord)
-})*/
-
-/*btnSearch.addEventListener('click', () => {
-    console.log('click')
-})*/
+})
 
 function randomPage(max) {
     return Math.floor(Math.random() * max);
